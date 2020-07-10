5e48a8830cf09ca5a0de9accd896e9a92fd1e647
Nanthini28 <nandhuactive28@gmail.com>
2020-07-10 08:30:54 +0530

    Commit in dev

--- empwage.sh
+++ empwage.sh
@@ -3,17 +3,33 @@ parttime=1;
 fulltime=2;
 rateperhr=20;
 empcheck=$((RANDOM%3))
+function myfun()
+{
+echo $1
+}
+result="$( myfun $((RANDOM%2)) )"
+
 echo "The case value:" $empcheck
 case $empcheck in
 	$fulltime)
 		emphrs=8
 		;;
 	$parttime)
+<<<<<<< HEAD
 		emphrs=5
+=======
+		emphrs=4
+>>>>>>> dev1
 		;;
 	*)
 	emphrs=0
 		;;
+esac
+case [ $result -eq 1 ]
+	echo "success"
+	;;
+	echo "failure"
+	;;
 esac
 salary=$(($emphrs*$rateperhr));
 echo "Total Salary:" $salary
